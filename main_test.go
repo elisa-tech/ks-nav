a2205dfffbd4ac761db35840fc63ab4a31732f1e
Alessandro Carminati <alessandro.carminati@gmail.com>
2022-10-11 12:28:10 +0200

    maintainers test

--- main_test.go
+++ main_test.go
@@ -6,6 +6,11 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"encoding/hex"
+	"archive/tar"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	)
 
 func TestParseFilesMakefile(t *testing.T){
@@ -75,3 +80,102 @@ func TestParseFilesConfig(t *testing.T){
 		}
 }
 
+func Untar(tarball, target string) error {
+	reader, err := os.Open(tarball)
+	if err != nil {
+		return err
+		}
+	defer reader.Close()
+	tarReader := tar.NewReader(reader)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+			} else if err != nil {
+				return err
+				}
+
+		path := filepath.Join(target, header.Name)
+		info := header.FileInfo()
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+				}
+			continue
+			}
+
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return err
+			}
+		_, err = io.Copy(file, tarReader)
+		if err != nil {
+			return err
+			}
+		 file.Close()
+		}
+	return nil
+}
+
+func cp(source string, destination string) error{
+
+	bytesRead, err := ioutil.ReadFile(source)
+	if err != nil {
+		return err
+		}
+	err = ioutil.WriteFile(destination, bytesRead, 0644)
+	if err != nil {
+		return err
+		}
+	return nil
+}
+
+
+func TestMaintainer(t *testing.T){
+	var FakeLinuxTreeTest string =	"t_files/linux-fake.tar"
+	var Fakedir string =		"/tmp/linux-fake"
+	var testData = []struct {
+		filename	string
+		subs		int
+		files		int
+		}{
+			{"t_files/linux-4.9.214_MAINTAINERS",	1627,113029},
+			{"t_files/linux-5.10.57_MAINTAINERS",	2251,135588},
+			{"t_files/linux-5.13.9_MAINTAINERS",	2336,136637},
+			{"t_files/linux-5.8.2_MAINTAINERS",	2208,133981},
+			{"t_files/linux-6.0_MAINTAINERS",	2585,142382},
+		}
+
+
+	defer os.RemoveAll(Fakedir)
+
+	err := Untar(FakeLinuxTreeTest, Fakedir)
+	if err!=nil {
+		t.Error("Error cant initialize fake linux directory", err)
+		}
+
+	current, _ := os.Getwd()
+	err = os.Chdir(Fakedir)
+	if err != nil {
+		t.Error("Error cant initialize fake linux directory", err)
+		}
+
+	for _,f := range testData {
+		err=cp(current+"/"+f.filename, "MAINTAINERS")
+		if err!=nil {
+			t.Error("Error cant use maintainer file", f.filename)
+			}
+		s, err := get_FromFile("MAINTAINERS")
+		if err!= nil {
+			t.Error("Error cant read maintainers", err)
+			}
+		ss:=s[seek2data(s):]
+		items:=parse_maintainers(ss)
+		queries:=generate_queries(items, "Subsystem: %[1]s' FileName='%[2]s",0)
+		if (f.subs!=len(items)) && (f.files!=len(queries)){
+			t.Error("Error validating number of files and subsystems in ", f.filename)
+			}
+		}
+
+}
