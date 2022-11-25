09ed7b2217bba5e84f51f71c3a6e197d67d2e9f9
Alessandro Carminati <alessandro.carminati@gmail.com>
2022-11-25 08:50:37 +0100

    gofmt applied to all files

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
-	/*
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 *   Name: kern_bin_db - Kernel source code analysis tool database creator
-	 *   Description: Parses kernel source tree and binary images and builds the DB
-	 *
-	 *   Author: Alessandro Carminati <acarmina@redhat.com>
-	 *   Author: Maurizio Papini <mpapini@redhat.com>
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 *   Copyright (c) 2022 Red Hat, Inc. All rights reserved.
-	 *
-	 *   This copyrighted material is made available to anyone wishing
-	 *   to use, modify, copy, or redistribute it subject to the terms
-	 *   and conditions of the GNU General Public License version 2.
-	 *
-	 *   This program is distributed in the hope that it will be
-	 *   useful, but WITHOUT ANY WARRANTY; without even the implied
-	 *   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-	 *   PURPOSE. See the GNU General Public License for more details.
-	 *
-	 *   You should have received a copy of the GNU General Public
-	 *   License along with this program; if not, write to the Free
-	 *   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-	 *   Boston, MA 02110-1301, USA.
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 */
+/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *   Name: kern_bin_db - Kernel source code analysis tool database creator
+ *   Description: Parses kernel source tree and binary images and builds the DB
+ *
+ *   Author: Alessandro Carminati <acarmina@redhat.com>
+ *   Author: Maurizio Papini <mpapini@redhat.com>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *   Copyright (c) 2022 Red Hat, Inc. All rights reserved.
+ *
+ *   This copyrighted material is made available to anyone wishing
+ *   to use, modify, copy, or redistribute it subject to the terms
+ *   and conditions of the GNU General Public License version 2.
+ *
+ *   This program is distributed in the hope that it will be
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied
+ *   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE. See the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the Free
+ *   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *   Boston, MA 02110-1301, USA.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 package main
 
 import (
@@ -37,32 +37,32 @@ import (
 
 	"github.com/cheggaaa/pb/v3"
 	r2 "github.com/radareorg/r2pipe-go"
-	)
+)
 
 // Bitfield configuration mode constants
 const (
-	ENABLE_SYBOLSNFILES	= 1
-	ENABLE_XREFS		= 2
-	ENABLE_MAINTAINERS	= 4
-	ENABLE_VERSION_CONFIG	= 8
-	)
+	ENABLE_SYBOLSNFILES   = 1
+	ENABLE_XREFS          = 2
+	ENABLE_MAINTAINERS    = 4
+	ENABLE_VERSION_CONFIG = 8
+)
 
-func main(){
-	var cache	[]xref_cache
-	var r2p		*r2.Pipe
-	var bar		*pb.ProgressBar
-	var funcs_data	[]func_data
-	var err		error
-	var count	int
-	var id		int
-    var addr2line_prefix string = ""
+func main() {
+	var cache []xref_cache
+	var r2p *r2.Pipe
+	var bar *pb.ProgressBar
+	var funcs_data []func_data
+	var err error
+	var count int
+	var id int
+	var addr2line_prefix string = ""
 
 	conf, err := args_parse(cmd_line_item_init())
-	if err!=nil {
+	if err != nil {
 		fmt.Println("Kernel symbol fetcher")
-		print_help(cmd_line_item_init());
+		print_help(cmd_line_item_init())
 		os.Exit(-1)
-		}
+	}
 	fmt.Println("create stripped version")
 	strip(conf.StripBin, conf.LinuxWDebug, conf.LinuxWODebug)
 	context := addr2line_init(conf.LinuxWDebug)
@@ -71,25 +71,25 @@ func main(){
 	if conf.Mode&(ENABLE_VERSION_CONFIG) != 0 {
 		config, _ := get_FromFile(conf.KConfig_fn)
 		makefile, _ := get_FromFile(conf.KMakefile)
-		v, err:= get_version(makefile)
-		if err!=nil {
+		v, err := get_version(makefile)
+		if err != nil {
 			panic(err)
-			}
+		}
 		fmt.Println(v)
-		q:=fmt.Sprintf("insert into instances (version_string, note) values ('%d.%d.%d%s', '%s');", v.Version, v.Patchlevel, v.Sublevel, v.Extraversion, conf.Note)
-		id=Insert_datawID(db, q)
-		kconfig:=parse_config(config)
+		q := fmt.Sprintf("insert into instances (version_string, note) values ('%d.%d.%d%s', '%s');", v.Version, v.Patchlevel, v.Sublevel, v.Extraversion, conf.Note)
+		id = Insert_datawID(db, q)
+		kconfig := parse_config(config)
 		fmt.Println("store config")
 		bar = pb.StartNew(len(kconfig))
-		for key,value :=range kconfig{
-			q:=fmt.Sprintf("insert into configs (config_symbol, config_value, config_instance_id_ref) values ('%s', '%s', %d);", key, value, id)
+		for key, value := range kconfig {
+			q := fmt.Sprintf("insert into configs (config_symbol, config_value, config_instance_id_ref) values ('%s', '%s', %d);", key, value, id)
 			bar.Increment()
-			spawn_query(db, 0, "None", context, q )
-			}
-		bar.Finish()
+			spawn_query(db, 0, "None", context, q)
 		}
+		bar.Finish()
+	}
 
-	if conf.Mode & (ENABLE_SYBOLSNFILES|ENABLE_XREFS) != 0 {
+	if conf.Mode&(ENABLE_SYBOLSNFILES|ENABLE_XREFS) != 0 {
 		r2p, err = r2.NewPipe(conf.LinuxWODebug)
 		if err != nil {
 			panic(err)
@@ -105,34 +105,34 @@ func main(){
 		fmt.Println("initialize analysis")
 		init_fw(r2p)
 		funcs_data = get_all_funcdata(r2p)
-		}
+	}
 
-	if conf.Mode & ENABLE_SYBOLSNFILES != 0 {
-		count=len(funcs_data)
+	if conf.Mode&ENABLE_SYBOLSNFILES != 0 {
+		count = len(funcs_data)
 		bar = pb.StartNew(count)
 
 		fmt.Println("collecting symbols & files")
-		for _, a :=range funcs_data{
+		for _, a := range funcs_data {
 			bar.Increment()
-			symbtype:="direct"
+			symbtype := "direct"
 			if a.Indirect {
-				symbtype="indirect"
-				}
+				symbtype = "indirect"
+			}
 			if strings.Contains(a.Name, "sym.") || a.Indirect {
-				fmtstring:=fmt.Sprintf(
-						"insert into files (file_name, file_instance_id_ref) Select '%%[1]s', %[1]d Where not exists "+
+				fmtstring := fmt.Sprintf(
+					"insert into files (file_name, file_instance_id_ref) Select '%%[1]s', %[1]d Where not exists "+
 						"(select * from files where file_name='%%[1]s' and file_instance_id_ref=%[1]d);"+
 						"insert into symbols (symbol_name, symbol_address, symbol_type, symbol_file_ref_id, symbol_instance_id_ref) "+
 						"select '%[2]s', '%[3]s', '%[4]s', (select file_id from files where file_name='%%[1]s' and file_instance_id_ref=%[1]d), %[1]d;"+
 						"",
-						id,
-						strings.ReplaceAll(a.Name, "sym.", ""),
-						fmt.Sprintf("0x%08x",a.Offset),
-						symbtype,
-						)
+					id,
+					strings.ReplaceAll(a.Name, "sym.", ""),
+					fmt.Sprintf("0x%08x", a.Offset),
+					symbtype,
+				)
 				spawn_query(
 					db,
-					a.Offset, 
+					a.Offset,
 					strings.ReplaceAll(a.Name, "sym.", ""),
 					context,
 					fmtstring)
@@ -149,24 +149,24 @@ func main(){
 					r := results[0]
 					start_kernel_file := filepath.Clean(r.File)
 					addr2line_prefix = start_kernel_file[:len(start_kernel_file)-len(start_kernel_file_tail)]
-				    } else {
+				} else {
 					fmt.Printf("\nWARNING: cannot get addr2line prefix tags results may be affected!\n")
-				    }
-			    }
-		    }
-		    bar.Finish()
-        }
-	if conf.Mode & ENABLE_XREFS != 0 {
+				}
+			}
+		}
+		bar.Finish()
+	}
+	if conf.Mode&ENABLE_XREFS != 0 {
 		fmt.Println("Collecting indrcalls")
-		indcl:=get_indirect_calls(r2p, funcs_data)
+		indcl := get_indirect_calls(r2p, funcs_data)
 		fmt.Println("Collecting xref")
 		bar = pb.StartNew(count)
-		for _, a :=range funcs_data{
+		for _, a := range funcs_data {
 			bar.Increment()
-			if strings.Contains(a.Name, "sym."){
+			if strings.Contains(a.Name, "sym.") {
 				Move(r2p, a.Offset)
-				xrefs:=remove_non_func(removeDuplicate(Getxrefs(r2p, a.Offset, indcl, funcs_data, &cache)),funcs_data)
-				for _, l :=range xrefs {
+				xrefs := remove_non_func(removeDuplicate(Getxrefs(r2p, a.Offset, indcl, funcs_data, &cache)), funcs_data)
+				for _, l := range xrefs {
 					spawn_query(
 						db,
 						0,
@@ -174,34 +174,34 @@ func main(){
 						context,
 						fmt.Sprintf(
 							"insert into xrefs (caller, callee, xref_instance_id_ref) "+
-							"select (Select symbol_id from symbols where symbol_address ='0x%08[1]x' and symbol_instance_id_ref=%[3]d), "+
-							"(Select symbol_id from symbols where symbol_address ='0x%08[2]x' and symbol_instance_id_ref=%[3]d), %[3]d;"+
-							"",
+								"select (Select symbol_id from symbols where symbol_address ='0x%08[1]x' and symbol_instance_id_ref=%[3]d), "+
+								"(Select symbol_id from symbols where symbol_address ='0x%08[2]x' and symbol_instance_id_ref=%[3]d), %[3]d;"+
+								"",
 							a.Offset,
 							l,
 							id))
-					}
 				}
 			}
-		bar.Finish()
 		}
+		bar.Finish()
+	}
 	tags_query := fmt.Sprintf("insert into tags (subsys_name, tag_file_ref_id, tag_instance_id_ref) select '%%[1]s', "+
 		"(select file_id from files where file_name='%[1]s%%[2]s' and file_instance_id_ref=%%[3]d) as fn_id, %%[3]d "+
 		"WHERE EXISTS ( select file_id from files where file_name='%[1]s%%[2]s' and file_instance_id_ref=%%[3]d);", addr2line_prefix)
-	if conf.Mode & ENABLE_MAINTAINERS != 0 {
+	if conf.Mode&ENABLE_MAINTAINERS != 0 {
 		fmt.Println("Collecting tags")
-		s,err:=get_FromFile(conf.Maintainers_fn)
-		if err!= nil {
+		s, err := get_FromFile(conf.Maintainers_fn)
+		if err != nil {
 			panic(err)
-			}
+		}
 		ss := s[seek2data(s):]
 		items := parse_maintainers(ss)
 		queries := generate_queries(conf.Maintainers_fn, items, tags_query, id)
 		bar = pb.StartNew(len(queries))
-		for _,q :=range queries{
+		for _, q := range queries {
 			bar.Increment()
-			spawn_query(db, 0, "None", context, q, )
-			}
-		bar.Finish()
+			spawn_query(db, 0, "None", context, q)
 		}
+		bar.Finish()
+	}
 }
