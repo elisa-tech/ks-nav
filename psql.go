d5e8daa6b45fec2c76f41f06f1115cd05f0d4345
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-01-03 21:43:04 +0100

    WIP - Refactoring: does not build

--- psql.go
+++ psql.go
@@ -36,14 +36,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const Insert_Instance_Q	string =	"insert into instances (version_string, note) values ('%d.%d.%d%s', '%s');"
-const Insert_Config_Q	string =	"insert into configs (config_symbol, config_value, config_instance_id_ref) values ('%s', '%s', %d);"
-const Insert_Files_Q	string =	"insert into files (file_name, file_instance_id_ref) select 'NoFile',%d;"
-const Insert_Symbols_Q	string =	"insert into symbols (symbol_name,symbol_address,symbol_type,symbol_file_ref_id,symbol_instance_id_ref) "+"select (select 'Indirect call'), '0x00000000', 'indirect', (select file_id from files where file_name ='NoFile' and file_instance_id_ref=%[1]d), %[1]d;"
-const Insert_Tags_Q	string =	"insert into tags (subsys_name, tag_file_ref_id, tag_instance_id_ref) select (select 'Indirect Calls'), "+"(select file_id from files where file_name='NoFile' and file_instance_id_ref=%[1]d), %[1]d;"
-const Insert_Mixed_Q	string =	"insert into files (file_name, file_instance_id_ref) Select '%%[1]s', %[1]d Where not exists "+"(select * from files where file_name='%%[1]s' and file_instance_id_ref=%[1]d);"+"insert into symbols (symbol_name, symbol_address, symbol_type, symbol_file_ref_id, symbol_instance_id_ref) "+"select '%[2]s', '%[3]s', '%[4]s', (select file_id from files where file_name='%%[1]s' and file_instance_id_ref=%[1]d), %[1]d;"
-const Insert_Xrefs_Q	string =	"insert into xrefs (caller, callee, ref_addr, source_line, xref_instance_id_ref) "+"select (Select symbol_id from symbols where symbol_address ='0x%08[1]x' and symbol_instance_id_ref=%[3]d), "+"(Select symbol_id from symbols where symbol_address ='0x%08[2]x' and symbol_instance_id_ref=%[3]d limit 1), "+"'0x%08[5]x', "+"'%[4]s', "+"%[3]d;"
-const Insert_Tags2_Q	string =	"insert into tags (subsys_name, tag_file_ref_id, tag_instance_id_ref) select '%%[1]s', "+"(select file_id from files where file_name='%[1]s%%[2]s' and file_instance_id_ref=%%[3]d) as fn_id, %%[3]d "+"WHERE EXISTS ( select file_id from files where file_name='%[1]s%%[2]s' and file_instance_id_ref=%%[3]d);"
 
 // Sql connection configuration
 type Connect_token struct {
@@ -52,6 +44,48 @@ type Connect_token struct {
 	User   string
 	Pass   string
 	Dbname string
+}
+
+type Context struct {
+        db              *sql.DB
+	Ch_workload	chan Workload
+
+type Insert_Instance_A struct {
+	Version		int
+	Patchlevel	int
+	Sublevel	int
+	Extraversion	string
+	Note		string
+}
+
+type Insert_Config_A struct {
+	Config_key	string
+	Config_val	string
+	Instance_no	int
+}
+type Insert_Files_Ind_A struct {
+	Id		int
+}
+type Insert_Symbols_Ind_A struct {
+	Id		int
+}
+type Insert_Tags_Ind_A struct {
+	Id		int
+}
+
+type Insert_Symbols_Files_A struct {
+	Id		int
+	Symbol_Name	string
+	Symbol_Offset	string
+	Symbol_Type	string
+}
+
+type Insert_Xrefs_A struct {
+	Caller_Offset	uint64
+	Calling_Offset	uint64
+	Callee_Offset	uint64
+	Id		int
+	Source_line	string
 }
 
 // Connects the target db and returns the handle
@@ -67,33 +101,29 @@ func Connect_db(t *Connect_token) *sql.DB {
 }
 
 // Executes insert queries
-func Insert_data(db *sql.DB, query string, test bool) {
+func Insert_data(context *Context, query string) {
 
-	if !test {
-		_, err := db.Exec(query)
-		if err != nil {
-			fmt.Println("##################################################")
-			fmt.Println(query)
-			fmt.Println("##################################################")
-			panic(err)
-		}
-	} else {
-		fmt.Println(query)
-	}
-}
-
-// Executes insert query for instance table and returns the id allocated
-func Insert_datawID(db *sql.DB, query string) int {
-	var res int
-
-	_, err := db.Exec(query)
+	_, err := (*context).db.Exec(query)
 	if err != nil {
 		fmt.Println("##################################################")
 		fmt.Println(query)
 		fmt.Println("##################################################")
 		panic(err)
 	}
-	rows, err := db.Query("SELECT currval('instances_instance_id_seq');")
+}
+
+// Executes insert query for instance table and returns the id allocated
+func Insert_datawID(context *Context, query string) int {
+	var res int
+
+	_, err := (*context).db.Exec(query)
+	if err != nil {
+		fmt.Println("##################################################")
+		fmt.Println(query)
+		fmt.Println("##################################################")
+		panic(err)
+	}
+	rows, err := (*context).db.Query("SELECT currval('instances_instance_id_seq');")
 	if err != nil {
 		panic(err)
 	}
