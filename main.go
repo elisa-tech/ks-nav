4cdb0ef621e0a46ef57db4cf4b3d006dd826aac3
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-06-15 14:17:01 +0200

    This commit implements a temporary solution for the issue #1 by reintroducing a dependency on the external dot utility from graphviz. This step is taken as an interim measure while waiting for the necessary fixes to be implemented upstream.
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-	"strconv"
+//	"strconv"
 	"fmt"
 )
 
@@ -39,7 +39,9 @@ func generateImageHandler(c *gin.Context) {
 		return
 	}
 
-	cmd := exec.Command("./nav/nav",  "-f", "confs/conf_sample.json", "-s", request.StartSymbol, "-i", strconv.Itoa(request.Instance), "-m", request.DisplayMode, "-x", request.Depth, "-g", "4", "-j","graphOnly")
+//	cmd := exec.Command("./nav/nav",  "-f", "confs/conf_sample.json", "-s", request.StartSymbol, "-i", strconv.Itoa(request.Instance), "-m", request.DisplayMode, "-x", request.Depth, "-g", "4", "-j","graphOnly")
+	cmdstr:=fmt.Sprintf("./nav/nav -f confs/conf_sample.json -s %s -i %d -m %s -x %s -g 1 -j graphOnly |dot -Tsvg", request.StartSymbol, request.Instance, request.DisplayMode, request.Depth)
+	cmd := exec.Command("/bin/bash", "-c", cmdstr)
 	fmt.Println("Executing command:", cmd.String())
 	output, err := cmd.Output()
 	if err != nil {
